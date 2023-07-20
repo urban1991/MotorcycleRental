bd896565f07cb962cb66968a38b497cbe1934bd2
urban1991 <piotr.urbaniak@tdsoft.pl>
2023-07-20 16:44:10 +0200

    server: change port number to less usable 4000

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require('./startup/validation')();
 require('./startup/prod')(app);
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const server = app.listen(port, ()=> logger.info(`listening on port ${port}...`));
 
 module.exports = server;
