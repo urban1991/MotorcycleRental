df047d088811076eb9517bc255b681687a8cd3ea
urban1991 <piotr.urbaniak@tdsoft.pl>
2023-07-22 22:37:52 +0200

    server: rewrite motorcycles.js router, create motorcycle.js file in controllers folder

---
+++ controllers/motorcycle.js
@@ -0,0 +1,102 @@
+const {Motorcycle, validate} = require("../models/motorcycle");
+const {Brand} = require("../models/brand");
+
+
+async function getAllMotorcycles(req, res) {
+  const motorcycles = await Motorcycle.find().sort("brand");
+  res.send(motorcycles);
+}
+
+async function getMotorcycle(req, res) {
+  const motorcycle = await Motorcycle.findById(req.params.id);
+  if (!motorcycle) {
+    return res.status(404).send("The motorcycle with given ID was not found");
+  }
+  res.send(motorcycle);
+}
+
+async function createMotorcycle(req, res) {
+  const {error} = validate(req.body);
+
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const motoBrand = await Brand.findById(req.body.brandId);
+
+  if (!motoBrand) {
+    return res.status(400).send("Invalid brand");
+  }
+
+  const motorcycle = await Motorcycle.create({
+    brand: {
+      _id: motoBrand._id,
+      brand: motoBrand.brand
+    },
+    model: req.body.model,
+    bodyType: req.body.bodyType,
+    motor: req.body.motor,
+    year: req.body.year,
+    dailyRentalFee: req.body.dailyRentalFee,
+    numberInStock: req.body.numberInStock,
+  });
+
+  res.send(motorcycle);
+}
+
+async function updateMotorcycle(req, res) {
+  const {error} = validate(req.body);
+
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const motoBrand = await Brand.findById(req.body.brandId);
+
+  if (!motoBrand) {
+    return res.status(400).send("Invalid brand");
+  }
+
+  const motorcycle = await Motorcycle.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        brand: {
+          _id: motoBrand._id,
+          brand: motoBrand.brand,
+        },
+        model: req.body.model,
+        bodyType: req.body.bodyType,
+        motor: req.body.motor,
+        year: req.body.year,
+        dailyRentalFee: req.body.dailyRentalFee,
+        numberInStock: req.body.numberInStock,
+      },
+    },
+    {returnOriginal: false},
+  );
+
+  if (!motorcycle) {
+    return res.status(404).send("The motorcycle with given ID was not found");
+  }
+
+  res.send(motorcycle);
+}
+
+async function deleteMotorcycle(req, res) {
+  const motorcycle = await Motorcycle.findByIdAndRemove(req.params.id);
+
+  if (!motorcycle) {
+    return res.status(404).send("The motorcycle with given ID was not found");
+  }
+
+  res.send(motorcycle);
+}
+
+module.exports = {
+  getAllMotorcycles,
+  getMotorcycle,
+  createMotorcycle,
+  updateMotorcycle,
+  deleteMotorcycle
+};
--- routes/motorcycles.js
+++ routes/motorcycles.js
@@ -1,83 +1,21 @@
-const mongoose = require("mongoose");
 const express = require("express");
-const auth = require("../middleware/auth");
-const {Motorcycle, validate} = require("../models/motorcycle");
-const {Mark} = require("../models/mark");
+const {
+  getAllMotorcycles,
+  createMotorcycle,
+  getMotorcycle,
+  updateMotorcycle,
+  deleteMotorcycle,
+} = require("../controllers/motorcycle");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const motorcycles = await Motorcycle.find().sort("mark");
-  res.send(motorcycles);
-});
+router.route("/")
+  .get(getAllMotorcycles)
+  .post(createMotorcycle);
 
-router.get("/:id", async (req, res) => {
-  const motorcycle = await Motorcycle.findById(req.params.id);
-  if (!motorcycle)
-    return res.status(404).send("The motorcycle with given ID was not found");
-  res.send(motorcycle);
-});
-
-router.post("/", auth, async (req, res) => {
-  const {error} = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const mark = await Mark.findById(req.body.markId);
-  if (!mark) return res.status(400).send("Invalid mark");
-
-  const motorcycle = new Motorcycle({
-    mark: {
-      _id: mark._id,
-      mark: mark.mark,
-    },
-    model: req.body.model,
-    bodyType: req.body.bodyType,
-    motor: req.body.motor,
-    year: req.body.year,
-    dailyRentalFee: req.body.dailyRentalFee,
-    numberInStock: req.body.numberInStock,
-  });
-
-  await motorcycle.save();
-  res.send(motorcycle);
-});
-
-router.put("/:id", auth, async (req, res) => {
-  const {error} = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const mark = await Mark.findById(req.body.markId);
-  if (!mark) return res.status(400).send("Invalid mark");
-
-  const motorcycle = await Motorcycle.findByIdAndUpdate(
-    req.params.id,
-    {
-      $set: {
-        mark: {
-          _id: mark._id,
-          mark: mark.mark,
-        },
-        model: req.body.model,
-        bodyType: req.body.bodyType,
-        motor: req.body.motor,
-        year: req.body.year,
-        dailyRentalFee: req.body.dailyRentalFee,
-        numberInStock: req.body.numberInStock,
-      },
-    },
-    {returnOriginal: false},
-  );
-  if (!motorcycle)
-    return res.status(404).send("The motorcycle with given ID was not found");
-  res.send(motorcycle);
-});
-
-router.delete("/:id", auth, async (req, res) => {
-  const motorcycle = await Motorcycle.findByIdAndRemove(req.params.id);
-  if (!motorcycle)
-    return res.status(404).send("The motorcycle with given ID was not found");
-
-  res.send(motorcycle);
-});
+router.route("/:id")
+  .get(getMotorcycle)
+  .patch(updateMotorcycle)
+  .delete(deleteMotorcycle);
 
 module.exports = router;
