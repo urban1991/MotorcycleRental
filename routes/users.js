436762072f63f8f654da8a11db7f6d132638e025
urban1991 <piotr.urbaniak@tdsoft.pl>
2023-07-22 17:05:29 +0200

    server:refactor users.js routs - extract function logic to controllers directory

--- routes/users.js
+++ routes/users.js
@@ -1,43 +1,10 @@
-const auth = require("../middleware/auth");
-const _ = require("lodash");
-const bcrypt = require("bcrypt");
-const {User, validate} = require("../models/user");
 const express = require("express");
+const {createUser, getUser} = require("../controllers/user");
 
 const router = express.Router();
-const Transaction = require("mongoose-transactions");
 
-const transaction = new Transaction();
+router.route("/me").get(getUser)
 
-router.get("/me", auth, async (req, res) => {
-  const user = await User.findById(req.user._id).select("-password -_id");
-  res.send(user);
-});
-
-router.post("/", async (req, res) => {
-  const {error} = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  let user = await User.findOne({email: req.body.email});
-  if (user)
-    return res.status(400).send("User with given email address  already exist");
-
-  user = new User(
-    _.pick(req.body, ["firstName", "lastName", "email", "password"]),
-  );
-  try {
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password, salt);
-    transaction.insert("User", user);
-    await transaction.run();
-
-    const token = user.generateAuthToken();
-    res
-      .header("x-auth-token", token)
-      .send(_.pick(user, ["firstName", "lastName", "email"]));
-  } catch (ex) {
-    res.status(500).send(ex);
-  }
-});
+router.route("/").post(createUser)
 
 module.exports = router;
