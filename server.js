4471cf53ebef47ef0e28e152f582ec68981173f0
urban1991 <piotr.urbaniak@tdsoft.pl>
2023-07-22 17:08:49 +0200

    server: rename file from index.js to server.js and add dotenv package

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 require("./middleware/logger");
-const winston = require("winston");
 const express = require("express");
 const {logger} = require("./middleware/error");
+
+const dotenv = require("dotenv");
+dotenv.config({path: "./.env"})
 
 const app = express();
 
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
-require("./startup/config")();
 require("./startup/validation")();
 require("./startup/prod")(app);
 
 const port = process.env.PORT || 4000;
+
 const server = app.listen(port, () =>
   logger.info(`listening on port ${port}...`),
 );
