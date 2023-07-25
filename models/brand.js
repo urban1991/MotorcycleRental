e4aa648c64a2e9bac1c6b0c8b970ce23451501d7
urban1991 <piotr.urbaniak@tdsoft.pl>
2023-07-25 18:27:31 +0200

    server: update brand.js model schema and validation

--- models/brand.js
+++ models/brand.js
@@ -8,11 +8,21 @@ const brandSchema = new Schema({
     type: String,
     required: true,
     minlength: 3,
-    maxlength: 50,
+    maxlength: 50
+  },
+  country: {
+    type: String,
+    required: false,
+    minlength: 2,
+    maxlength: 50
+  },
+  website: {
+    type: String,
+    required: false
   },
   createdAt: {
     type: Date,
-    default: Date.now(),
+    default: Date.now()
   }
 });
 
@@ -21,6 +31,8 @@ const Brand = mongoose.model("Brand", brandSchema);
 function validateBrand(brand) {
   const schema = Joi.object({
     brand: Joi.string().min(3).max(50).required(),
+    country: Joi.string().min(2).max(50),
+    website: Joi.string().uri()
   });
 
   return schema.validate(brand);
