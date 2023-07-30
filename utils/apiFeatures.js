2b97e6303808844e4e7b24a6927a94bdeeaa2d86
urban1991 <piotr.urbaniak@tdsoft.pl>
2023-07-30 10:00:23 +0200

    server: add sort, limitFields, paginate methods to apiFeatures.js class

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -5,11 +5,10 @@ class ApiFeatures {
   }
 
   filter() {
-    const queryObj = { ...this.queryString };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    const queryObj = {...this.queryString};
+    const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach(el => delete queryObj[el]);
 
-    // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
@@ -18,5 +17,39 @@ class ApiFeatures {
     return this;
   }
 
-  sort()
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortBy);
+
+    } else {
+      this.query = this.query.sort("-createdAt");
+    }
+
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
+
+    } else {
+      this.query = this.query.select("-__v");
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = Number(this.queryString.page) || 1;
+    const limit = Number(this.queryString.limit) || 100;
+
+    const skippedRecords = (page - 1) * limit;
+    this.query = this.query.skip(skippedRecords).limit(limit);
+
+    return this;
+  }
 }
+
+module.exports = ApiFeatures;
