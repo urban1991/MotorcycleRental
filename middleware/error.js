aba1bc552dce90d177a29ae06c01a81814470577
urban1991 <piotr.urbaniak@tdsoft.pl>
2023-07-30 21:38:35 +0200

    server: fix broken logger

--- middleware/error.js
+++ middleware/error.js
@@ -3,33 +3,24 @@ require("winston-mongodb");
 
 const logger = winston.createLogger({
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({filename: "logfile.log"}),
-
-    // new winston.transports.MongoDB(
-    //     {
-    //         db: 'mongodb://localhost/motoRental',
-    //         level: 'info',
-    //         options: {
-    //             useUnifiedTopology: true
-    //         },
-    //         metaKey: 'meta',
-    //         storeHost: true,
-    //     })
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.prettyPrint()
+      )
+    }),
+    new winston.transports.File({filename: "logfile.log"})
   ],
   exceptionHandlers: [
     new winston.transports.File({
       filename: "exceptions.log",
-      handleExceptions: true,
-    }),
-  ],
+      handleExceptions: true
+    })
+  ]
 });
 
-module.exports = function (err, req, res, next) {
+module.exports = function(err, req, res) {
   logger.error(err.message, {meta: err});
-  logger.transports.Console({colorize: true, prettyPrint: true});
-  logger.exceptions.handle(new transports.File({filename: "exceptions.log"}));
-
   res.status(500).send("Something failed terribly.");
 };
 
