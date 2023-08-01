63f63df670c532773e1624880e8350339c7ae1eb
urban1991 <piotr.urbaniak@tdsoft.pl>
2023-08-01 21:02:56 +0200

    server: update Joi validation motorcycle function to make required fields optional in update controller

--- models/motorcycle.js
+++ models/motorcycle.js
@@ -67,24 +67,24 @@ const motorcycleSchema = new Schema({
 
 const Motorcycle = mongoose.model("Motorcycle", motorcycleSchema);
 
-function validateMotorcycle(motorcycle) {
+function validateMotorcycle(motorcycle, requestType) {
   const schema = Joi.object({
     brandId: Joi.string().required(),
-    model: Joi.string().min(3).max(150).required(),
-    bodyType: Joi.string().valid("sport", "enduro", "tourist", "chopper").required(),
-    motor: Joi.number().min(49).max(3000).required(),
-    year: Joi.number().integer().min(1885).max(new Date().getFullYear()).required(),
+    model: Joi.string().min(3).max(150).alter({patch: schema => schema.optional()}),
+    bodyType: Joi.string().valid("sport", "enduro", "tourist", "chopper").required().alter({patch: schema => schema.optional()}),
+    motor: Joi.number().min(49).max(3000).required().alter({patch: schema => schema.optional()}),
+    year: Joi.number().integer().min(1885).max(new Date().getFullYear()).required().alter({patch: schema => schema.optional()}),
     imageUrl: Joi.string().uri().allow(""),
-    dailyRentalFee: Joi.number().min(0).max(3000).required(),
-    numberInStock: Joi.number().min(0).max(15).required(),
-    mileage: Joi.number().required(),
+    dailyRentalFee: Joi.number().min(0).max(3000).required().alter({patch: schema => schema.optional()}),
+    numberInStock: Joi.number().min(0).max(15).required().alter({patch: schema => schema.optional()}),
+    mileage: Joi.number().required().alter({patch: schema => schema.optional()}),
     description: Joi.string().allow(""),
-    VIN: Joi.string().required(),
+    VIN: Joi.string().required().alter({patch: schema => schema.optional()}),
     createdAt: Joi.date()
   });
 
 
-  return schema.validate(motorcycle);
+  return schema.tailor(requestType).validate(motorcycle);
 }
 
 exports.Motorcycle = Motorcycle;
