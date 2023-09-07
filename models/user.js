e338abb184551453e17e45b67b32ed530b468022
urban1991 <piotr.urbaniak@tdsoft.pl>
2023-09-07 19:58:58 +0200

    server: minor fixes, added user schema salting and hashing password using bcrypt

--- models/user.js
+++ models/user.js
@@ -1,35 +1,36 @@
 const mongoose = require("mongoose");
 // const jwt = require("jsonwebtoken");
 const Joi = require("joi");
+const bcrypt = require("bcrypt");
 
 const {Schema} = mongoose;
 
 const userSchema = new Schema({
   firstName: {
     type: String,
-    required: true,
     minlength: 3,
     maxlength: 50,
+    required: true,
   },
   lastName: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 50,
+    required: true,
   },
   email: {
     type: String,
-    required: true,
-    unique: true,
     minlength: 5,
     maxlength: 255,
+    unique: true,
+    required: true,
   },
   password: {
     type: String,
     minlength: 5,
     maxlength: 1024,
-    required: true,
     select: false,
+    required: true,
   },
   confirmPassword: {
     type: String,
@@ -37,11 +38,16 @@ const userSchema = new Schema({
     maxlength: 1024,
     required: true,
   },
+  driverLicenseNumber: {
+    type: String,
+    unique: true,
+    required: true,
+  },
   phoneNumber: {
     type: String,
-    required: false,
     minlength: 10,
     maxlength: 15,
+    required: false,
   },
   dateOfBirth: {
     type: Date,
@@ -49,18 +55,13 @@ const userSchema = new Schema({
   },
   address: {
     type: String,
-    required: false,
     minlength: 5,
     maxlength: 255,
+    required: false,
   },
   avatarUrl: {
     type: String,
     required: false,
-  },
-  driverLicenseNumber: {
-    type: String,
-    required: true,
-    unique: true,
   },
   isVerified: {
     type: Boolean,
@@ -71,6 +72,16 @@ const userSchema = new Schema({
     default: Date.now(),
   },
   isAdmin: Boolean,
+});
+
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  // Hash the password with cost of 12 only if it has been modified or is new
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+  next();
 });
 
 // userSchema.methods.generateAuthToken = function () {
@@ -102,6 +113,7 @@ function validateUser(user, requestType) {
       .min(5)
       .max(1024)
       .alter({patch: (schema) => schema.optional()}),
+    confirmPassword: Joi.ref("password"),
     phoneNumber: Joi.string().min(10).max(15),
     dateOfBirth: Joi.date(),
     address: Joi.string().min(5).max(255),
