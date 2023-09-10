e58d79212fb256abfb2da3c0e8eb2f29afb6de81
urban1991 <piotr.urbaniak@tdsoft.pl>
2023-09-10 20:21:03 +0200

    server: add isAuthorized.js middleware

---
+++ middleware/isAuthorized.js
@@ -0,0 +1,12 @@
+const AppError = require("../utils/AppError");
+
+const isAuthorized = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(new AppError("You don't have permissions to access this resources", 403));
+    }
+    next();
+  };
+};
+
+module.exports = {isAuthorized};
