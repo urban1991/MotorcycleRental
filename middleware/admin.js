d90e941c79a127ecf4d207685b8b71424244777d
urban1991 <urban199191@gmail.com>
2023-09-23 20:53:22 +0200

    server: format code

--- middleware/admin.js
+++ middleware/admin.js
@@ -2,6 +2,5 @@ module.exports = function (req, res, next) {
   if (!req.user.isAdmin) {
     return res.status(403).send("Access denied");
   }
-
   next();
 };
--- middleware/isAuthorized.js
+++ middleware/isAuthorized.js
@@ -1,17 +1,17 @@
 const AppError = require("../utils/AppError");
 
 const isAuthorized =
-  (...roles) =>
-  (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return next(
-        new AppError(
-          "You don't have permissions to access this resources",
-          403,
-        ),
-      );
-    }
-    next();
-  };
+    (...roles) =>
+        (req, res, next) => {
+            if (!roles.includes(req.user.role)) {
+                return next(
+                    new AppError(
+                        "You don't have permissions to access this resources",
+                        403,
+                    ),
+                );
+            }
+            next();
+        };
 
 module.exports = {isAuthorized};
