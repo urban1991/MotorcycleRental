e18024ecdc8347aa49a312ad8706d6709f6fe55a
urban1991 <urban199191@gmail.com>
2023-09-25 19:37:09 +0200

    server: format code

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,11 +1,11 @@
 //TODO: check if this is still needed, for now there is no use of it
 const validate = (validator) => (req, res, next) => {
-  const {error} = validator(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
+    const {error} = validator(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
 
-  next();
+    next();
 };
 
 module.exports = validate;
